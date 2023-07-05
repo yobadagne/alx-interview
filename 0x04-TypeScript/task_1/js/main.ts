f6e59ddbcb4860a82d4e38f80f5b379d69469d39
yobadagne <yobadagne@gmail.com>
2023-07-05 13:53:21 +0000

    4

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-;export interface Teacher {
+export interface Teacher {
   readonly firstName: string,
   readonly lastName: string,
   fullTimeEmployee: boolean,
@@ -7,13 +7,41 @@
   [index:string]: any,
 }
 
-export interface Director extend Teacher {
+export interface Director extends Teacher {
   numberOfReport: number,
 }
+
 export interface printTeacherFunction {
-  (firstName: string. lastName: string): string
+  (firstName: string, lastName: string): string;
 }
 
-export function printTeacher(firtsName: string, lastName: string): string {
-  return `${firstName.slice(0)}. ${lastName}`;
+export function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`;
+}
+
+export interface IStudentClassConstructor {
+  new (firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export class StudentClass implements IStudentClass {
+  private _firstName!: string;
+  private _lastName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
 }
