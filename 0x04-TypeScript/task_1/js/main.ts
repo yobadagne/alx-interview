02be6893c1989e902826e3af3a997975ababe384
yobadagne <yobadagne@gmail.com>
2023-07-05 13:44:16 +0000

    2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,11 @@
   location: string,
   [index:string]: any,
 }
+
+export interface printTeacherFunction {
+  (firstName: string. lastName: string): string
+}
+
+export function printTeacher(firtsName: string, lastName: string): string {
+  return `${firstName.slice(0)}. ${lastName}`;
+}
