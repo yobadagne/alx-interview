5208a5e0482b86f92ae4913b55ae4e480808dc1d
yobadagne <yobadagne@gmail.com>
2023-07-30 11:25:46 +0000

    0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" UTTF-8 validation""" 
+def validUTF8(data):
+    bytes = 1
+
+    for i in data:
+        if bytes == 1:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                bytes = 2
+            elif i >> 4 == 0b1110:
+                bytes= 3
+            elif i >> 3 == 0b11110:
+                bytes = 4
+            elif i >> 7 == 0b1:
+                return False
+        else:
+            if i >> 6 != 0b10:
+                return False
+            bytes -= 1
+    return bytes == 1
