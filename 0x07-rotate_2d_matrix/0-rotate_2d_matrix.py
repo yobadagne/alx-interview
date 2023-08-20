a3ae4db73b87a197ec1061b1bf5f6ba8aa0e1762
yobadagne <yobadagne@gmail.com>
2023-08-20 09:49:39 +0000

    2d

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+    l = len(matrix)
+    for i in range(int(l / 2)):
+        y = (l - i - 1)
+        for j in range(i, y):
+            x = (l - 1 - j)
+            # current number
+            tmp = matrix[i][j]
+            # change top for left
+            matrix[i][j] = matrix[x][i]
+            # change left for bottom
+            matrix[x][i] = matrix[y][x]
+            # change bottom for right
+            matrix[y][x] = matrix[j][y]
+            # change right for top
+            matrix[j][y] = tmp
