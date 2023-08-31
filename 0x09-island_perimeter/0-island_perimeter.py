92ea4869bfbc51cfd225b00635dab2ba285a7509
yobadagne <yobadagne@gmail.com>
2023-08-31 14:51:57 +0000

    island

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""land Perimeter Problem
+"""
+
+
+def island_perimeter(grid):
+    """
+    Calculates the perimeter of the island described in grid
+    Args:
+        grid: 2d list of integers containing 0(water) or 1(land)
+    Return:
+        the perimeter of the island
+    """
+
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if (grid[i][j] == 1):
+                if (i <= 0 or grid[i - 1][j] == 0):
+                    count += 1
+                if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
+                    count += 1
+                if (j <= 0 or grid[i][j - 1] == 0):
+                    count += 1
+                if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
+                    count += 1
+    return count
